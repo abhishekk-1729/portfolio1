73101963949cedf04f2975c24ba222fa85c8c873
abhishekk-1729 <135116930+abhishekk-1729@users.noreply.github.com>
2024-01-23 03:45:56 +0530

    Update Register.js

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -30,7 +30,7 @@ export default function Register() {
         event.preventDefault();
         // first time regsister krte h jab tb page reload krdeta h
         try {
-            const response = await fetch('http://localhost:8000/api/auth/register',{
+            const response = await fetch('https://abhishek.nssiitd.in/portfolio/api/auth/register',{
                 method:"POST",
                 headers:{
                     'Content-Type':"application/json"
