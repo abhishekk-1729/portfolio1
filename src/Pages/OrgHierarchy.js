c17292f98da852c8f1aa3372c8385805f9967990
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-01-24 13:59:02 +0530

    leaernings updae

--- src/Pages/OrgHierarchy.js
+++ src/Pages/OrgHierarchy.js
@@ -5,41 +5,15 @@ export default function OrgHierarchy() {
     <>
       <div className="projectDescription">
             <h1>
-                MERN Web Developement
+                Organization Hierarchy
             </h1>
-            <div className="projectImg">
-                <img src="/images/mern.png" alt="loading" height="500" width ="1000" />
-            </div>
             <p className="subheading">
-                What is Web Developement
+                Project Description
             </p>
             <p className="text">
                 Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something
             </p>
-            <p className="subheading">
-                What is Web Developement
-            </p>
-            <p className="text">
-                Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something
-            </p>
-            <p className="subheading">
-                What is Web Developement
-            </p>
-            <p className="text">
-                Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something
-            </p>
-            <p className="subheading">
-                What is Web Developement
-            </p>
-            <p className="text">
-                Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something
-            </p>
-            <p className="subheading">
-                What is Web Developement
-            </p>
-            <p className="text">
-                Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something Web development is the art of doing something
-            </p>
+
         </div>
     </>
   )
